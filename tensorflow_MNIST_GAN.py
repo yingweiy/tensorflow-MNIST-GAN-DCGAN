cf9994ca40360fad1e5a96b19d836b005ca316c8
Yingwei Yu <yingweiy@gmail.com>
2018-06-06 10:03:45 -0500

    Fix a bug in MNIST_GAN

--- tensorflow_MNIST_GAN.py
+++ tensorflow_MNIST_GAN.py
@@ -53,7 +53,7 @@ def discriminator(x, drop_out):
 
     # 3rd hidden layer
     w2 = tf.get_variable('D_w2', [h1.get_shape()[1], 256], initializer=w_init)
-    b2 = tf.get_variable('D_b2', [1024], initializer=b_init)
+    b2 = tf.get_variable('D_b2', [256], initializer=b_init)
     h2 = tf.nn.relu(tf.matmul(h1, w2) + b2)
     h2 = tf.nn.dropout(h2, drop_out)
 
